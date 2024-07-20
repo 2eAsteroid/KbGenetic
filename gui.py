bd8d8f8eae6f4a741407567e2f3eb04a3162cd35
2e Asteroid <two-e-asteroid@outlook.com>
2024-07-20 17:43:27 -0400

    Initial commit

---
+++ gui.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+
+def initiate_gui() -> None:
+    root = Tk()
+    root.title("Simple Calendar")
+
+    root.mainloop()
---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+import gui
+
+def main(argv) -> int:
+    gui.initiate_gui()
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
